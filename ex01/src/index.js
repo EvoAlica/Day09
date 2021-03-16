35ece5aaa36ae0d481bb640446be66a6053f310e
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-16 16:00:22 +0100

    Added all Ex01-Ex18

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+// Only change code above this line
+function myTrueBoolean() {
+    return true;
+}
+console.log(myTrueBoolean());
+
+// Only change code above this line
+module.exports = myTrueBoolean;
